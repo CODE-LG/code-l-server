725b01d242c3b894561bfa71c4f75a1ab632f747
GS_song98 <20003204@sju.ac.kr>
2025-04-23 16:06:44 +0900

    chore: mysql rds 연결 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 
     // db
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
 
     // test
