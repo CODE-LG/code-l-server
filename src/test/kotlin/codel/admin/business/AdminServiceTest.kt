0963aa3d554099f5a456d76d260749be2014363d
GS_song98 <20003204@sju.ac.kr>
2025-04-24 01:27:39 +0900

    test: 승인, 거절 테스트 추가

--- src/test/kotlin/codel/admin/business/AdminServiceTest.kt
+++ src/test/kotlin/codel/admin/business/AdminServiceTest.kt
@@ -1,14 +1,15 @@
 package codel.admin.business
 
 import codel.admin.domain.Admin
-import codel.admin.domain.ValidateCode
 import codel.admin.exception.AdminException
 import codel.config.TestFixture
 import codel.member.business.MemberService
-import codel.member.domain.Member
 import codel.member.domain.MemberStatus
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -26,14 +27,14 @@ class AdminServiceTest(
     @Test
     fun loginAdminSuccessTest() {
         val admin = Admin(password = answerPassword)
-        Assertions.assertDoesNotThrow { adminService.loginAdmin(admin) }
+        assertDoesNotThrow { adminService.loginAdmin(admin) }
     }
 
     @DisplayName("정확하지 않은 비밀번호를 작성하면 로그인에 실패한다.")
     @Test
     fun loginAdminFailureTest() {
         val admin = Admin(password = "wrong password")
-        Assertions.assertThrows(AdminException::class.java) { adminService.loginAdmin(admin) }
+        assertThrows(AdminException::class.java) { adminService.loginAdmin(admin) }
     }
 
     @DisplayName("PENDING 상태인 회원을 찾아온다.")
@@ -41,47 +42,33 @@ class AdminServiceTest(
     fun findPendingMembersTest() {
         val pendingMembers = adminService.findPendingMembers()
 
-        Assertions.assertAll(
-            { Assertions.assertEquals(1, pendingMembers.size) },
-            { Assertions.assertEquals(memberPending.id, pendingMembers[0].id) },
+        assertAll(
+            { assertEquals(1, pendingMembers.size) },
+            { assertEquals(memberPending.id, pendingMembers[0].id) },
         )
     }
 
     @DisplayName("사용자의 프로필을 승인하면 사용자의 상태가 DONE 으로 바뀐다.")
     @Test
     fun reviewMemberProfileApproveTest() {
-        val target =
-            Member(
-                oauthType = memberPending.oauthType,
-                oauthId = memberPending.oauthId,
-            )
+        adminService.approveMemberProfile(memberPending.id!!)
 
-        adminService.reviewMemberProfile(target, ValidateCode.APPROVE)
-
-        val findMember =
-            memberService.findMember(
-                oauthType = memberPending.oauthType,
-                oauthId = memberPending.oauthId,
-            )
+        val findMember = memberService.findMember(memberId = memberPending.id!!)
         assertThat(findMember.memberStatus).isEqualTo(MemberStatus.DONE)
     }
 
     @DisplayName("사용자의 프로필을 거절하면 사용자의 상태가 REJECT 로 바뀐다.")
     @Test
     fun reviewMemberProfileRejectTest() {
-        val target =
-            Member(
-                oauthType = memberPending.oauthType,
-                oauthId = memberPending.oauthId,
-            )
+        val rejectReason = "페이스 이미지에 얼굴이 제대로 확인되지 않습니다."
 
-        adminService.reviewMemberProfile(target, ValidateCode.REJECT)
+        adminService.rejectMemberProfile(memberPending.id!!, rejectReason)
 
-        val findMember =
-            memberService.findMember(
-                oauthType = memberPending.oauthType,
-                oauthId = memberPending.oauthId,
-            )
-        assertThat(findMember.memberStatus).isEqualTo(MemberStatus.REJECT)
+        val findMember = memberService.findMember(memberId = memberPending.id!!)
+
+        assertAll(
+            { assertThat(findMember.memberStatus).isEqualTo(MemberStatus.REJECT) },
+            { assertThat(findMember.rejectReason).isEqualTo(rejectReason) },
+        )
     }
 }
