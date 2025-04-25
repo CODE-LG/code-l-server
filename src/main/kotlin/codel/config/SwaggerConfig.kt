4a44a2e844a56259ddf73a3da63ace44d6a3f134
GS_song98 <20003204@sju.ac.kr>
2025-04-25 23:26:54 +0900

    feat: swagger 토큰 기능 추가

--- src/main/kotlin/codel/config/SwaggerConfig.kt
+++ src/main/kotlin/codel/config/SwaggerConfig.kt
@@ -1,9 +1,12 @@
 package codel.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -26,5 +29,17 @@ class SwaggerConfig {
                             .name("Apache 2.0")
                             .url("http://www.apache.org/licenses/LICENSE-2.0"),
                     ),
+            ).components(
+                Components()
+                    .addSecuritySchemes(
+                        "accessToken",
+                        SecurityScheme()
+                            .name("Authorization")
+                            .type(SecurityScheme.Type.APIKEY)
+                            .`in`(SecurityScheme.In.HEADER)
+                            .bearerFormat("JWT"),
+                    ),
+            ).addSecurityItem(
+                SecurityRequirement().addList("accessToken"),
             )
 }
