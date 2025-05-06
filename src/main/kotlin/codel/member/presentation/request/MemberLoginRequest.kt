e52a86a407d20d77c5b82c9673a31dee48d5550b
hoyeonyy <highoption@naver.com>
2025-05-06 15:19:57 +0900

    bug: email nullable 수정

--- src/main/kotlin/codel/member/presentation/request/MemberLoginRequest.kt
+++ src/main/kotlin/codel/member/presentation/request/MemberLoginRequest.kt
@@ -6,12 +6,12 @@ import codel.member.domain.OauthType
 data class MemberLoginRequest(
     val oauthType: OauthType,
     val oauthId: String,
-    val email: String,
+    val email: String?,
 ) {
     fun toMember(): Member =
         Member(
             oauthType = this.oauthType,
             oauthId = this.oauthId,
-            email = this.email,
+            email = this.email ?: "",
         )
 }
