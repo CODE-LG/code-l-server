c344b09f42b0262420b230d7ecda6329d915da1a
GS_song98 <20003204@sju.ac.kr>
2025-07-05 13:45:00 +0900

    refactor: BaseTimeEntity 추가

--- src/main/kotlin/codel/chat/domain/ChatRoom.kt
+++ src/main/kotlin/codel/chat/domain/ChatRoom.kt
@@ -1,12 +1,12 @@
 package codel.chat.domain
 
+import codel.common.domain.BaseTimeEntity
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
-import java.time.LocalDateTime
 
 @Entity
 class ChatRoom(
@@ -16,8 +16,6 @@ class ChatRoom(
     @OneToOne
     @JoinColumn(name = "chat_id")
     var recentChat: Chat? = null,
-    var createdAt: LocalDateTime? = null,
-    var updatedAt: LocalDateTime? = null,
-) {
+) : BaseTimeEntity() {
     fun getIdOrThrow(): Long = id ?: throw IllegalStateException("채팅방이 존재하지 않습니다.")
 }
