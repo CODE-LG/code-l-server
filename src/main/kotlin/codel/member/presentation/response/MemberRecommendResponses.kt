6a937e607c2d02c6746902a808cee51acd18159e
sgo722 <sgo722@naver.com>
2025-07-14 17:10:36 +0900

    refactor : 배열 형태의 응답 데이터 일관성 수정

--- src/main/kotlin/codel/member/presentation/response/MemberRecommendResponses.kt
+++ src/main/kotlin/codel/member/presentation/response/MemberRecommendResponses.kt
@@ -3,7 +3,7 @@ package codel.member.presentation.response
 import codel.member.domain.Member
 
 data class MemberRecommendResponses(
-    val responses: List<MemberResponse>,
+    val content: List<MemberResponse>,
 ) {
     companion object {
         fun toResponse(members: List<Member>): MemberRecommendResponses {
