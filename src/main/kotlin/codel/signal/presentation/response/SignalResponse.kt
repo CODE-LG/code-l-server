a1c8185ad170bd323408da1ac9b88e337670b557
sgo722 <sgo722@naver.com>
2025-07-14 20:44:37 +0900

    [feat] 시그널 전송 응답 값 fcmToken 추가

--- src/main/kotlin/codel/signal/presentation/response/SignalResponse.kt
+++ src/main/kotlin/codel/signal/presentation/response/SignalResponse.kt
@@ -6,14 +6,16 @@ data class SignalResponse(
     val id: Long,
     val fromMemberId: Long,
     val toMemberId: Long,
-    val status : String
+    val status : String,
+    val toMemberFcmToken: String?,
 ) {
     companion object {
         fun from(signal: Signal) = SignalResponse(
             id = signal.id!!,
             fromMemberId = signal.fromMember.id!!,
             toMemberId = signal.toMember.id!!,
-            status = signal.status.toString()
+            status = signal.status.toString(),
+            toMemberFcmToken = signal.toMember.fcmToken
         )
     }
 } 
