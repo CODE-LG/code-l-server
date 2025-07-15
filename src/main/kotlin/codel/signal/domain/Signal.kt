96d9e9638b88d0349dea9c0b9a819afcd2a97f4e
sgo722 <sgo722@naver.com>
2025-07-15 14:02:11 +0900

    [fix] SignalStatus 상태 추가 : Hidden 을 PENDING_HIDDEN / ACCEPTED_HIDDEN 로 구분

--- src/main/kotlin/codel/signal/domain/Signal.kt
+++ src/main/kotlin/codel/signal/domain/Signal.kt
@@ -32,7 +32,7 @@ class Signal(
     fun validateSendable(now: LocalDateTime = LocalDateTime.now()) {
         if (!canSendNewSignal(now)) {
             when (status) {
-                SignalStatus.PENDING, SignalStatus.ACCEPTED, SignalStatus.HIDDEN ->
+                SignalStatus.PENDING, SignalStatus.ACCEPTED, SignalStatus.PENDING_HIDDEN, SignalStatus.ACCEPTED_HIDDEN ->
                     throw SignalException(HttpStatus.BAD_REQUEST, "이미 시그널을 보낸 상대입니다.")
                 SignalStatus.REJECTED ->
                     throw SignalException(HttpStatus.BAD_REQUEST, "거절된 상대에게는 7일 후에 다시 시그널을 보낼 수 있습니다.")
@@ -42,7 +42,7 @@ class Signal(
 
     fun canSendNewSignal(now: LocalDateTime = LocalDateTime.now()): Boolean {
         return when (status) {
-            SignalStatus.PENDING, SignalStatus.ACCEPTED, SignalStatus.HIDDEN -> false
+            SignalStatus.PENDING, SignalStatus.ACCEPTED, SignalStatus.PENDING_HIDDEN, SignalStatus.ACCEPTED_HIDDEN -> false
             SignalStatus.REJECTED -> updatedAt.plusDays(7).isBefore(now)
         }
     }
