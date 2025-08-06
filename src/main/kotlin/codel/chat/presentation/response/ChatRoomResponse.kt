cdece537b613e958f04774c2eb9402c414a8b10e
sgo722 <sgo722@naver.com>
2025-08-06 21:32:58 +0900

    [feat] 채팅방 목록 조회 DTO 수정 : 채팅방 상태 포함시켜서 반환

--- src/main/kotlin/codel/chat/presentation/response/ChatRoomResponse.kt
+++ src/main/kotlin/codel/chat/presentation/response/ChatRoomResponse.kt
@@ -1,6 +1,7 @@
 package codel.chat.presentation.response
 
 import codel.chat.domain.ChatRoom
+import codel.chat.domain.ChatRoomStatus
 import codel.member.domain.Member
 import codel.member.presentation.response.MemberResponse
 import java.time.LocalDateTime
@@ -11,6 +12,7 @@ data class ChatRoomResponse(
     val partner: MemberResponse,
     val lastChatId : Long?,
     val recentChat: ChatResponse?,
+    val chatRoomStatus : ChatRoomStatus,
     val createdAt: LocalDateTime,
     val updatedAt: LocalDateTime,
 ) {
@@ -28,6 +30,7 @@ data class ChatRoomResponse(
                 lastChatId = lastChatId,
                 recentChat = chatRoom.recentChat?.let { ChatResponse.toResponse(requester, it) },
                 unReadMessageCount = unReadMessageCount,
+                chatRoomStatus = chatRoom.status,
                 createdAt = chatRoom.createdAt,
                 updatedAt = chatRoom.updatedAt,
             )
