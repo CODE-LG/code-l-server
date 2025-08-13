3a98befda601104a787441507a9ae2ea940ac0e5
sgo722 <sgo722@naver.com>
2025-08-13 15:27:37 +0900

    [bug] 내 메시지를 제외한 메시지 카운팅하도록 수정

--- src/main/kotlin/codel/chat/infrastructure/ChatJpaRepository.kt
+++ src/main/kotlin/codel/chat/infrastructure/ChatJpaRepository.kt
@@ -2,6 +2,7 @@ package codel.chat.infrastructure
 
 import codel.chat.domain.Chat
 import codel.chat.domain.ChatRoom
+import codel.member.domain.Member
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
@@ -30,6 +31,7 @@ interface ChatJpaRepository : JpaRepository<Chat, Long> {
             SELECT count(c) from Chat c
             WHERE c.chatRoom = :chatRoom
             AND c.sentAt > :afterTime
+            AND c.fromChatRoomMember.member != :requester
             AND (
                 c.senderType = 'USER' 
                 OR c.chatContentType IN ('CODE_QUESTION', 'CODE_UNLOCKED_REQUEST')
@@ -39,18 +41,20 @@ interface ChatJpaRepository : JpaRepository<Chat, Long> {
     fun countByChatRoomAfterLastChat(
         chatRoom: ChatRoom,
         afterTime: LocalDateTime,
+        requester: Member,
     ): Int
 
     @Query(
         """
         SELECT count(c) from Chat c
         WHERE c.chatRoom = :chatRoom
+        AND c.fromChatRoomMember.member != :requester
         AND (
             c.senderType = 'USER' 
             OR c.chatContentType IN ('CODE_QUESTION', 'CODE_UNLOCKED_REQUEST')
         )
     """)
-    fun countByChatRoomAfterLastChat(chatRoom: ChatRoom): Int
+    fun countByChatRoomAfterLastChat(chatRoom: ChatRoom, requester: Member): Int
 
     @Query("""
     SELECT c
