37d486f910d55af8cdc13d9f8cc4363964361d04
sgo722 <sgo722@naver.com>
2025-08-13 16:05:44 +0900

    [bug] recentChatTime 타입 변경 : LocalDateTime -> LocalDateTime, 안드로이드와의 호환 문제 및 LocalDateTime으로 받지않아도 처리 가능함

--- src/main/kotlin/codel/chat/presentation/request/ChatSendRequest.kt
+++ src/main/kotlin/codel/chat/presentation/request/ChatSendRequest.kt
@@ -1,11 +1,11 @@
 package codel.chat.presentation.request
 
 import codel.chat.domain.ChatSenderType
-import java.time.LocalDateTime
+import java.time.LocalDate
 
 data class ChatSendRequest(
     val message: String,
     val memberId: Long,
     val chatType: ChatSenderType,
-    val recentChatTime: LocalDateTime,
+    val recentChatTime: LocalDate,
 )
